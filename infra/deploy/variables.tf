356abc76685a82090779dac375b93cd8c1432049
nagappanarun <arun.nagappan@yudrio.com>
2024-06-08 17:35:42 -0400

    added Terraform

---
+++ infra/deploy/variables.tf
@@ -0,0 +1,14 @@
+variable "prefix" {
+  description = "Prefix name for tagging resources."
+  default     = "raa"
+}
+
+variable "project" {
+  description = "Project name for tagging resources."
+  default     = "recipe-app-api"
+}
+
+variable "contact" {
+  description = "Contact email for tagging resources."
+  default     = "arun.nagappan@yudrio.com"
+}
